5c98778eecf136aa0b63603d6a63935437af09de
Alena <apispallada@gmail.com>
2022-08-06 11:42:03 +0300

    HW_4 is done

---
+++ HW_4/Task_2.py
@@ -0,0 +1,15 @@
+'''
+Задайте натуральное число N. Напишите программу, которая составит список 
+простых делителей числа N. (1 - составное число)
+'''
+num = int(input("Введите число: "))
+print("Простые делители " + str(num) + " -> ", end = " ")
+for i in range(num, 0, -1):
+    simple_div = 0
+    if num % i == 0:
+        for j in range(i-1, 1, -1):
+            if i % j == 0:
+                simple_div += 1
+        if simple_div == 0:
+            print(i, end = " ")
+ 
